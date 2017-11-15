ed83f1a95cf0b2ddbd3c873c2553eebb97f1132a
Tammy Cravit <tammycravit@users.noreply.github.com>
2017-11-15 10:01:27 -0800

    Update ChildrenEx.lua
    Output list is now returned sorted, can be overridden by providing some value for args[2].

--- ChildrenEx.lua
+++ ChildrenEx.lua
@@ -6,8 +6,13 @@
 -- Usage:
 -- 
 -- {{lua ChildrenEx.lua,CategoryName}}
+--
+-- {{lua ChildrenEx.lua,CategoryName,Unsorted}}
 
 search_for = args[1]
+unsorted = args[2]
+
+list_items = {}
 
 a = wiki.search(search_for .. ":")
 the_output = ""
@@ -23,7 +28,17 @@ for i, name in pairs(a) do
 		page_title = page_title:gsub("([%u]+)", " %1")
 		page_title = page_title:gsub("([%d]+)", " %1")
 		page_title = page_title:gsub("^%s*(.-)%s*$", "%1")
-		the_output = the_output .. "- [[" .. name .. "|" .. page_title .. "]]\n"
+		list_entry = "- [[" .. name .. "|" .. page_title .. "]]"
+		table.insert(list_items, list_entry)
 	end
 end
+
+if not unsorted then
+	table.sort(list_items)
+end
+
+for i, item in ipairs(list_items) do
+	the_output = the_output .. item .. "\n"
+end
+
 return the_output
