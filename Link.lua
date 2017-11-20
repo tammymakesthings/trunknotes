4ef81087d1fa123c73ccfe2fbb98108b4f8b3cdf
Tammy Cravit <tammycravit@users.noreply.github.com>
2017-11-20 08:33:40 -0800

    Fixed for pages that have nested categories
    Fixed to now work correctly with pages that have a multi-level category (Category:Subcategory:PageName). For these kinds of links, the link_target was not being properly parsed.

--- Link.lua
+++ Link.lua
@@ -1,6 +1,10 @@
 -- Automagically create a link with a title, so you dont have to type
 -- [[PageCategory:PageName|Page Name]] over and over.
 
+Updated Fri Nov 17, 2017 - 16:04 
+-- Automagically create a link with a title, so you dont have to type
+-- [[PageCategory:PageName|Page Name]] over and over.
+
 function string:split( inSplitPattern, outResults )
   if not outResults then
     outResults = { }
@@ -21,16 +25,24 @@ if args[2] then
 	page_title = args[2]
 else
 	page_title = args[1]
-
-	if link_target:match(":") then
-		toks = link_target:split(":")
-		link_target = toks[1]
-	end
-
-	page_title = page_title:gsub("([%u]+)", " %1")
-	page_title = page_title:gsub("([%d]+)", " %1")
-	page_title = page_title:gsub("^%s*(.-)%s*$", "%1")
 end
 
-return "[[" .. link_target .. ":" .. args[1] .. "|" .. page_title .. "]]"
+if link_target:match(":") then
+	b = ""
+	toks = link_target:split(":")
+	for i, tag in ipairs(toks) do
+		if i == 1 then
+			b = toks[i]
+		elseif i < #toks then
+			b = b .. ":" .. toks[i]
+		end
+	end
+	link_target = b
+end
 
+page_title = page_title:gsub("([%u]+)", " %1")
+page_title = page_title:gsub("([%d]+)", " %1")
+page_title = page_title:gsub("^%s*(.-)%s*$", "%1")
+
+
+return "[[" .. link_target .. ":" .. args[1] .. "|" .. page_title .. "]]"
